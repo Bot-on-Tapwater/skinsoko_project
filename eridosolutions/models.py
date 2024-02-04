0f85b3c84887e3606a85bbf8b6fb2708cbcbb342
Bot-on-Tapwater <mundabrandon@outlook.com>
2024-02-04 10:02:03 +0300

    Update some fields to not be nullable

--- eridosolutions/models.py
+++ eridosolutions/models.py
@@ -17,11 +17,11 @@ class Category(models.Model):
 
 class Product(models.Model):
     product_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    quantity_in_stock = models.PositiveIntegerField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, null=False)
+    description = models.TextField(null=False)
+    price = models.PositiveIntegerField(null=False)
+    quantity_in_stock = models.PositiveIntegerField(null=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=False)
     image = models.ImageField(upload_to='images', null=True, blank=True)
 
     def __str__(self):
@@ -73,7 +73,7 @@ class CartItem(models.Model):
 class Order(models.Model):
     order_id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    total_amount = models.PositiveIntegerField()
     order_status = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -94,7 +94,7 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
-    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    unit_price = models.PositiveIntegerField()
 
     def __str__(self):
         return f'Item ID: {self.item_id} - Order: {self.order.order_id} - Product: {self.product.name}'
