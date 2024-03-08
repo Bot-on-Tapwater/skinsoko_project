60b938a68124f497706b3a0719e6c802a86e92cf
Bot-on-Tapwater <mundabrandon@outlook.com>
2024-03-08 17:19:08 +0300

    REFACTOR: Remove user ID's from api routes

--- eridosolutions/urls.py
+++ eridosolutions/urls.py
@@ -25,22 +25,28 @@ urlpatterns = [
     path('products/<int:id>/delete/', views.delete_product_with_product_id, name='delete-product'),
 
     # User Profile
-    path('users/<int:id>/', views.get_user_with_user_id_profile_details, name='get-user-profile-details'),
-    path('users/<int:id>/update/', views.update_user_with_user_id_profile_details, name='update-user-profile-details'),
-    path('users/<int:id>/orders/', views.list_orders_placed_by_user_with_user_id, name='list-user-orders'),
+    # path('users/<int:id>/', views.get_user_with_user_id_profile_details, name='get-user-profile-details'),
+    path('users/', views.get_user_with_user_id_profile_details, name='get-user-profile-details'),
+    # path('users/<int:id>/update/', views.update_user_with_user_id_profile_details, name='update-user-profile-details'),
+    path('users/update/', views.update_user_with_user_id_profile_details, name='update-user-profile-details'),
+    # path('users/<int:id>/orders/', views.list_orders_placed_by_user_with_user_id, name='list-user-orders'),
+    path('users/orders/', views.list_orders_placed_by_user_with_user_id, name='list-user-orders'),
 
     # Shopping Cart
-    path('user/cart/', views.get_contents_of_shopping_cart_of_user, name='get-cart-contents'),
+    path('users/cart/', views.get_contents_of_shopping_cart_of_user, name='get-cart-contents'),
     # path('users/<int:id>/cart/', views.get_contents_of_shopping_cart_of_user, name='get-cart-contents'),
-    path('user/cart/add/<int:productId>/', views.add_product_to_user_cart, name='add-product-to-cart'),
+    path('users/cart/add/<int:productId>/', views.add_product_to_user_cart, name='add-product-to-cart'),
     # path('users/<int:id>/cart/add/<int:productId>/', views.add_product_to_user_cart, name='add-product-to-cart'),
-    path('users/<int:id>/cart/remove/<int:productId>/', views.remove_product_from_user_cart, name='remove-product-from-cart'),
-    path('users/<int:id>/cart/clear/', views.clear_entire_shopping_cart, name='clear-cart'),
+    # path('users/<int:id>/cart/remove/<int:productId>/', views.remove_product_from_user_cart, name='remove-product-from-cart'),
+    path('users/cart/remove/<int:productId>/', views.remove_product_from_user_cart, name='remove-product-from-cart'),
+    # path('users/<int:id>/cart/clear/', views.clear_entire_shopping_cart, name='clear-cart'),
+    path('users/cart/clear/', views.clear_entire_shopping_cart, name='clear-cart'),
 
     # Order Management
     path('orders/', views.get_list_of_all_orders, name='list-all-orders'),
     path('orders/<int:id>/', views.get_details_of_order_with_order_id, name='get-order-details'),
-    path('users/<int:userId>/orders/create/', views.create_new_order, name='create-new-order'),
+    # path('users/<int:userId>/orders/create/', views.create_new_order, name='create-new-order'),
+    path('users/orders/create/', views.create_new_order, name='create-new-order'),
     path('orders/<int:id>/cancel/', views.cancel_order_with_order_id, name='cancel-order'),
 
     # Payment Integration
@@ -56,7 +62,8 @@ urlpatterns = [
 
     # Reviews and Ratings
     path('products/<int:id>/reviews/', views.get_reviews_for_product_with_product_id, name='get-product-reviews'),
-    path('users/<int:userId>/products/<int:id>/reviews/create/', views.creat_review_for_product_with_product_id, name='create-product-review'),
+    # path('users/<int:userId>/products/<int:id>/reviews/create/', views.creat_review_for_product_with_product_id, name='create-product-review'),
+    path('users/products/<int:id>/reviews/create/', views.creat_review_for_product_with_product_id, name='create-product-review'),
 
     # Search and Filters
     path('search/', views.search_products, name='search-products'),
@@ -64,10 +71,14 @@ urlpatterns = [
 
     # Shipping and Address
     path('shipping-options/', views.get_available_shipping_options, name='get-shipping-options'),
-    path('users/<str:userId>/addresses/', views.get_user_saved_addresses, name='get-user-addresses'),
-    path('users/<str:userId>/addresses/create/', views.add_address_to_user_profile, name='add-address-to-profile'),
-    path('users/<str:userId>/addresses/<int:id>/update/', views.update_details_of_address_with_address_id, name='update-address-details'),
-    path('users/<str:userId>/addresses/<int:id>/delete/', views.delete_address_with_address_id, name='delete-address'),
+    # path('users/<str:userId>/addresses/', views.get_user_saved_addresses, name='get-user-addresses'),
+    path('users/addresses/', views.get_user_saved_addresses, name='get-user-addresses'),
+    # path('users/<str:userId>/addresses/create/', views.add_address_to_user_profile, name='add-address-to-profile'),
+    path('users/addresses/create/', views.add_address_to_user_profile, name='add-address-to-profile'),
+    # path('users/<str:userId>/addresses/<int:id>/update/', views.update_details_of_address_with_address_id, name='update-address-details'),
+    path('users/addresses/<int:id>/update/', views.update_details_of_address_with_address_id, name='update-address-details'),
+    # path('users/<str:userId>/addresses/<int:id>/delete/', views.delete_address_with_address_id, name='delete-address'),
+    path('users/addresses/<int:id>/delete/', views.delete_address_with_address_id, name='delete-address'),
 
     # Error handling
     path('error/', views.error),
