b9bcda00ff87cfe1f795e7a5a2b5b3add1990b52
Bot-on-Tapwater <mundabrandon@outlook.com>
2024-06-09 15:29:20 +0300

    update: add variables for smtp

--- django_apps/settings.py
+++ django_apps/settings.py
@@ -175,3 +175,12 @@ CSRF_COOKIE_SECURE = True
 
 # CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]
 # LOGIN_REDIRECT_URL = ''
+
+# Email & SMTP
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'brandonmunda1@gmail.com'
+EMAIL_HOST_PASSWORD = 'wfte hrcq cuua gqag'
