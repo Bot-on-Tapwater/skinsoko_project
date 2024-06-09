2c705ea36b53f15549ff3263ca4f44d9347f741b
Bot-on-Tapwater <mundabrandon@outlook.com>
2024-06-09 17:17:42 +0300

    feat: enable all models for django admin dashboard

--- skinsoko/admin.py
+++ skinsoko/admin.py
@@ -1,3 +1,17 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from .models import User, MainCategory, SubCategory, Brand, Product, ShoppingCart, CartItem, Order, OrderItem, Review, Address, Towns, Wishlist
+
+admin.site.register(User)
+admin.site.register(MainCategory)
+admin.site.register(SubCategory)
+admin.site.register(Brand)
+admin.site.register(Product)
+admin.site.register(ShoppingCart)
+admin.site.register(CartItem)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Review)
+admin.site.register(Address)
+admin.site.register(Towns)
+admin.site.register(Wishlist)
