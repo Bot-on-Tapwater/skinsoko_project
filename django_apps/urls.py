2d61a25b3838fea87de4d5b8ebfcb208642c8fd9
Bot-on-Tapwater <mundabrandon@outlook.com>
2024-06-14 14:21:30 +0300

    update: Add urls for django social auth

--- django_apps/urls.py
+++ django_apps/urls.py
@@ -18,10 +18,17 @@ from django.urls import path
 from django.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('skinsoko/', include("skinsoko.urls")),
     path('eridosolutions/', include("eridosolutions.urls")),
     path('admin/', admin.site.urls),
+
+    # Login and Logout
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+
+    path('oauth/', include('social_django.urls', namespace='social')),
 ]
 
