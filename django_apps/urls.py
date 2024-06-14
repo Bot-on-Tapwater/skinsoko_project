2f70cd3f50492eebbf2e42670332f1f2dc9520af
Bot-on-Tapwater <mundabrandon@outlook.com>
2024-06-14 14:55:32 +0300

    fix: Comment out login and logout paths

--- django_apps/urls.py
+++ django_apps/urls.py
@@ -26,9 +26,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # Login and Logout
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+    # path('login/', auth_views.LoginView.as_view(), name='login'),
+    # path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
 
-    path('oauth/', include('social_django.urls', namespace='social')),
+    path('social-auth/', include('social_django.urls', namespace='social')),
 ]
 
