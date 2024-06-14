1ef91e33ab5974773a0cacbf1b4241cf9742c3db
Bot-on-Tapwater <mundabrandon@outlook.com>
2024-06-14 14:55:03 +0300

    update: Insert facebook ID and KEY

--- django_apps/settings.py
+++ django_apps/settings.py
@@ -204,3 +204,24 @@ AUTHENTICATION_BACKENDS = (
 
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
+
+SOCIAL_AUTH_FACEBOOK_KEY = '7678214898922660'  
+SOCIAL_AUTH_FACEBOOK_SECRET = '11d9c5430fc9f5e6d8d0382dd9fd448d' 
+
+SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/'
+SOCIAL_AUTH_LOGIN_ERROR_URL = '/login-error/'
+SOCIAL_AUTH_LOGIN_URL = '/login/'
+SOCIAL_AUTH_LOGOUT_URL = '/logout/'
+SOCIAL_AUTH_REDIRECT_IS_HTTPS = True  # If your site uses HTTPS
+
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
